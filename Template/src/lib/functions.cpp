10d500501bc2f01999e820afc71367ea1299ed7d
root <root@KTB-debian.am.omron.net>
2018-05-24 18:15:49 -0400

    Template Ready

---
+++ Template/src/lib/functions.cpp
@@ -0,0 +1,13 @@
+// Created by Kaustubh Shivdikar
+//
+// (C) All Rights Reserved
+
+
+#include "functions.h"
+#include "debugger.h"
+
+#include <string>
+
+using namespace std;
+
+
---
+++ Template/src/lib/functions.h
@@ -0,0 +1,23 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <iostream>
+#include <stdio.h>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <array>
+#include <cmath>
+#include <fstream>
+
+
+
+
+
+
+
+
+
+
+
+#endif // FUNCTIONS_H
