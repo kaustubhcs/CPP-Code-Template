3b559aa116739aabe8db05bafd580af48c6e4305
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2021-09-10 10:58:06 -0400

    COLOR MACROS added

--- Template/src/code/main.cpp
+++ Template/src/code/main.cpp
@@ -2,29 +2,23 @@
 //
 //  (C) All Rights Reserved
 
-
-# include "../lib/functions.h"
-# include "../lib/debugger.h"
+#include "../lib/functions.h"
+#include "../lib/debugger.h"
 
 using namespace std;
 
 // Global Variables
 
-
-
-
-
 // ************ MAIN FUNCTION ************
 
+int main(int argc, char *argv[])
+{
 
-int main (int argc, char *argv[]) {
+	Input input;
+	get_inputs(argc, argv, input, 1);
 
-		Input input;
-		get_inputs (argc, argv, input, 1);
+	cout << "I am King of the " << YELLOW_START << "World" << COLOR_RESET << "!" << endl;
 
-
-
-		cout << "\n\e[1;31mProgram End\e[0m\n\n\n";
-		return 0;
+	cout << "\n\e[1;31mProgram End\e[0m\n\n\n";
+	return 0;
 }
-
--- Template/src/lib/debugger.h
+++ Template/src/lib/debugger.h
@@ -5,12 +5,18 @@
 #ifndef DEBUGGER_H
 #define DEBUGGER_H
 
+#define RED_START "\e[1;31m"
+#define GREEN_START "\e[1;32m"
+#define YELLOW_START "\e[1;33m"
+#define CYAN_START "\e[1;36m"
+#define UNDERLINE_START "\e[1;4m"
+#define COLOR_RESET "\e[0m"
+
 void red_start();
 void green_start();
 void yellow_start();
 void cyan_start();
 void underline_start();
 void color_reset();
-
 
 #endif // DEBUGGER_H
